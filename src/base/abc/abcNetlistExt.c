a0601b4284b3daee0e4dec10462a390b1c3f0878
VMCentOS-00 <VMCentOS-00@VMCentOS-00.(none)>
2017-02-22 23:19:15 +0800

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   src/base/abc/abcNetlistExt.c

--- src/base/abc/abcNetlistExt.c
+++ src/base/abc/abcNetlistExt.c
@@ -34,6 +34,7 @@ ABC_NAMESPACE_IMPL_START
 ////////////////////////////////////////////////////////////////////////
 
 Abc_Ntk_t * Abc_NtkToLogicExt( Abc_Ntk_t * pNtk, int fDeFloating, int FloatingSig );
+Abc_Ntk_t * Abc_NtkToLogicDefloating( Abc_Ntk_t * pNtk );
 
 /**Function*************************************************************
 
@@ -75,6 +76,60 @@ Abc_Ntk_t * Abc_NtkToLogicExt( Abc_Ntk_t * pNtk, int fDeFloating, int FloatingSi
                 Abc_ObjAddFanin( pObj->pCopy, (FloatingSig == 0)? Abc_NtkCreateNodeConst0(pNtkNew): Abc_NtkCreateNodeConst1(pNtkNew) );
             else
                 Abc_ObjAddFanin( pObj->pCopy, Abc_ObjFanin0(pFanin)->pCopy );
+        }
+    // collect the CO nodes
+    Abc_NtkFinalize( pNtk, pNtkNew );
+    // fix the problem with CO pointing directly to CIs
+    Abc_NtkLogicMakeSimpleCos( pNtkNew, 0 );
+    // duplicate EXDC 
+    if ( pNtk->pExdc )
+        pNtkNew->pExdc = Abc_NtkToLogic( pNtk->pExdc );
+    if ( !Abc_NtkCheck( pNtkNew ) )
+        fprintf( stdout, "Abc_NtkToLogic(): Network check has failed.\n" );
+    return pNtkNew;
+}
+
+/**Function*************************************************************
+
+  Synopsis    [Transform the netlist into a logic network, and set floating signal to inputs]
+
+  Description []
+               
+  SideEffects []
+
+  SeeAlso     []
+
+***********************************************************************/
+Abc_Ntk_t * Abc_NtkToLogicDefloating( Abc_Ntk_t * pNtk )
+{
+    Abc_Ntk_t * pNtkNew; 
+    Abc_Obj_t * pObj, * pFanin;
+    int i, k;
+    // consider the case of the AIG
+    if ( Abc_NtkIsStrash(pNtk) )
+        return Abc_NtkToLogic( pNtk );
+    assert( Abc_NtkIsNetlist(pNtk) );
+    // consider simple case when there is hierarchy
+//    assert( pNtk->pDesign == NULL );
+    assert( Abc_NtkWhiteboxNum(pNtk) == 0 );
+    assert( Abc_NtkBlackboxNum(pNtk) == 0 );
+    // start the network
+    pNtkNew = Abc_NtkStartFrom( pNtk, ABC_NTK_LOGIC, pNtk->ntkFunc );
+    // duplicate the nodes 
+    Abc_NtkForEachNode( pNtk, pObj, i )
+    {
+        Abc_NtkDupObj(pNtkNew, pObj, 0);
+        Abc_ObjAssignName( pObj->pCopy, Abc_ObjName(Abc_ObjFanout0(pObj)), NULL );
+    }
+
+    // reconnect the internal nodes in the new network
+    Abc_NtkForEachNode( pNtk, pObj, i )
+        Abc_ObjForEachFanin( pObj, pFanin, k ){
+            assert ( Abc_ObjIsNet(pFanin) );
+            if ( Abc_ObjFaninNum(pFanin) != 0 )
+                Abc_ObjAddFanin( pObj->pCopy, Abc_ObjFanin0(pFanin)->pCopy );
+            else
+                Abc_ObjAddFanin( pObj->pCopy, Abc_NtkCreatePi( pNtkNew ) );
         }
     // collect the CO nodes
     Abc_NtkFinalize( pNtk, pNtkNew );
